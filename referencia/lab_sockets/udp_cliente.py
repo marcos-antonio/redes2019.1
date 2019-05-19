1cc587d6281470a50f83f8c9d8e39b3387ebefe0
Marcos Antonio <mantonioprof@gmail.com>
2019-05-19 19:10:04 -0300

    Chg udp_cliente
        - Adiciona registro de usuario
        - Adiciona disposicao de comandos
        - Adiciona comando enviar mensagem
        - Adiciona comando listar usuarios

--- referencia/lab_sockets/udp_cliente.py
+++ referencia/lab_sockets/udp_cliente.py
@@ -1,29 +1,73 @@
 import socket
+import threading
 from datetime import datetime
 
 ENCODE = "UTF-8"
 HOST = '127.0.0.1'   # Endereco IP do Servidor
 PORT = 5000          # Porta que o Servidor esta
 MAX_BYTES = 65535    # Quantidade de Bytes a serem ser recebidos
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverDest = (HOST, PORT)
 
 def client():
-    """ Procedimento responsavel por enviar dados para o servidor e receber alguma resposta por conta disso """
+    username = registerUsername()
+    while True:
+        print('Comandos possiveis\n')
+        print('1 - Enviar mensagem\n')
+        print('2 - Listar usuarios\n')
+        comando = int(raw_input('Digite o numero do comando desejado: \n'))
 
-    text = input("Digite algum texto:\n")       # Recebe dados
-    data = text.encode(ENCODE)				    # Codifica para BASE64 os dados de entrada	
-    
-    #Enviando de dados
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Inicializar um socket UDP
-    dest = (HOST, PORT)                                     # Define IP de origem e Porta de destino  
-    sock.sendto(data, dest)                                 # Envia os dados para o destino
+        if comando == 1:
+            messageSender()
+        elif comando == 2:
+            printUserlist()
+        else:
+            print('Comando invalido')
 
-    #Resposta de envio ao servidor
-    print(sock.getsockname())				    # Imprime dados do socker de destino
-    data, address = sock.recvfrom(MAX_BYTES)    # Recebendo dados
-    text = data.decode(ENCODE)                  # Convertendo dados de BASE64 para UTF-8
-    print(address, text)                        # Imprime texto e enderecos
 
-    #Fechando Socket
-    sock.close()
-    
+def registerUsername():
+    isAccepted = False
+    username = ''
+    while isAccepted == False:
+        username = raw_input('Digite seu nome de usuario:\n')
+        encodedUsername = username.encode(ENCODE)
+
+        sock.sendto(username, serverDest)
+
+        data, address = sock.recvfrom(MAX_BYTES)
+        response = data.decode(ENCODE)
+
+        if response != 'Usuario ja cadastrado':
+            isAccepted = True
+
+    return username
+
+def messageSender():
+    message = raw_input('Digite a mensagem que voce quer enviar:\n')
+    receiver = raw_input('Digite o nome do usuario receptor:\n')
+    encodedData = (message + ':' + receiver).encode(ENCODE)
+
+    sock.sendto(encodedData, serverDest)
+
+    data, address = sock.recvfrom(MAX_BYTES)
+    response = data.decode(ENCODE)
+
+    print(response)
+
+def resolveServerMessages():
+    data, address = sock.recvfrom(MAX_BYTES)
+    response = data.decode(ENCODE)
+
+    print(response)
+
+def printUserlist():
+
+    sock.sendto('1'.encode(ENCODE), serverDest)
+    data, address = sock.recvfrom(MAX_BYTES)
+    response = str(data.decode(ENCODE))
+
+    userlist = response.split(':')
+    for u in userlist:
+        print(u)
+
 client()
