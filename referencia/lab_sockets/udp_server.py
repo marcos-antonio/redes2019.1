5d89d8776e07c9753fd06e857fc9cb7a8debc51e
Marcos Antonio <mantonioprof@gmail.com>
2019-05-19 21:42:02 -0300

    Add registro visualizacao e envio de mensagem server

--- referencia/lab_sockets/udp_server.py
+++ referencia/lab_sockets/udp_server.py
@@ -3,24 +3,58 @@ from datetime import datetime
 
 ENCODE = "UTF-8"
 MAX_BYTES = 65535
-PORT = 5000            # Porta que o Servidor esta
-HOST = ''     	       # Endereco IP do Servidor
+PORT = 5000
+HOST = ''
+userlist = {}
+orig = (HOST, PORT)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(orig)
 
 def server():
-    #Abrindo um socket UDP na porta 5000
-    orig = (HOST, PORT)																
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(orig)
-
     while True:
-        #recebi dados
-        data, address = sock.recvfrom(MAX_BYTES) # Recebi dados do socket
-        text = data.decode(ENCODE)               # Convertendo dados de BASE64 para UTF-8
-        print(address, text)
+        data, address = sock.recvfrom(MAX_BYTES)
+        data = data.decode(ENCODE).split(':')
+        text = ''
+        code = 5
+        action = data[0]
+        data = eval(data[1])
 
-        #Envia resposta
-        text = "Total de dados recebidos: " + str(len(data)) 
-        data = text.encode(ENCODE) # Codifica para BASE64 os dados 
-        sock.sendto(data, address) # Enviando dados	
+
+        if (action == '1'):
+            if registerUser(data[0], address) == True:
+                text = 'Usuario cadastrado com sucesso'
+                code = 2
+            else:
+                code = 4
+                text = 'Usuario ja cadastrado'
+        elif (action == '2'):
+            code = 2
+            text = str(list(userlist.keys()))
+        elif (action == '3'):
+            if sendMessage(data[0], data[1]) == True:
+                code = 2
+                text = 'Mensagem enviada'
+            else:
+                code = 4
+                text = 'Usuario nao encontrado'
+        text = (str(code) + ':' + text).encode(ENCODE)
+        sock.sendto(text, address)
+
+
+def registerUser(username, data):
+    if userlist.get(username) == None:
+        userlist[username] = data
+        return True
+    else:
+        return False
+
+def sendMessage(message, destiny):
+    destinyAddress = userlist.get(destiny)
+    if destinyAddress == None:
+        return False
+    else:
+        message = ('2:' + message).encode(ENCODE)
+        sock.sendto(message, destinyAddress)
+        return True
 
 server()
